9640829471d763b5a358091cdab0dddb41983b42
Julian Maurice <julian.maurice@biblibre.com>
2023-11-06 14:33:58 +0100

    Fix parameters passed to ConfigForm constructor

--- src/Service/Form/ConfigFormFactory.php
+++ src/Service/Form/ConfigFormFactory.php
@@ -12,7 +12,7 @@ class ConfigFormFactory implements FactoryInterface
     {
         $settings = $services->get('Omeka\Settings');
 
-        $form = new ConfigForm([], $options ?? []);
+        $form = new ConfigForm(null, $options ?? []);
         $form->setGlobalSettings($settings);
 
         return $form;
